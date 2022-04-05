6c4c04958a4229f6b4bc154cfda21fcdbaced5fb
Yuri <YuriE1989@yandex.ru>
2022-04-05 16:11:58 +0300

    Start project

---
+++ scripts/loadDisp.js
@@ -0,0 +1,256 @@
+//данные для демо
+let gases = {
+    title: "Концентрации",
+    values: [
+        { isSelect: true, title: "CO" },
+        { isSelect: true, title: "CO2" },
+        { isSelect: true, title: "C2H2" },
+        { isSelect: true, title: "C2H4" },
+        { isSelect: true, title: "C2H6" },
+        { isSelect: true, title: "H" },
+        { isSelect: true, title: "CH4" }
+    ]
+}
+
+let workParams = {
+    title: "Рабочие параметры",
+    values: [
+        { title: "Т ВСМ" },
+        { title: "Температура наружняя" },
+        { title: "Т НСМ" },
+    ]
+}
+
+let data = [gases, workParams];
+
+let dataForTable =[
+    { title: "CO", ppm: getRandomArbitrary(0, 300), percent: getRandomArbitrary(0, 300), DZ:100, PDZ:150},
+    { title: "CO2", ppm: getRandomArbitrary(0, 300), percent: getRandomArbitrary(0, 300), DZ: 3000, PDZ: 7000 },
+    { title: "C2H2", ppm: getRandomArbitrary(0, 300), percent: getRandomArbitrary(0, 300), DZ: 8, PDZ: 20 },
+    { title: "C2H4", ppm: getRandomArbitrary(0, 300), percent: getRandomArbitrary(0, 300), DZ: 20, PDZ: 50 },
+    { title: "C2H6", ppm: getRandomArbitrary(0, 300), percent: getRandomArbitrary(0, 300), DZ: 35, PDZ: 80 },
+    { title: "H", ppm: getRandomArbitrary(0, 300), percent: getRandomArbitrary(0, 300), DZ: 80, PDZ: 100 },
+    { title: "CH4", ppm: getRandomArbitrary(0, 300), percent: getRandomArbitrary(0, 300), DZ: 40, PDZ: 70 }
+]
+
+
+
+
+function loadDisp(titleDisp) {
+
+    console.log(dataForTable);
+    $('.innerContiner').empty();
+    createBtnCallLeftMenu();
+    createTabsComponents();
+    createDemoChart();
+}
+
+//Создание компонентов вкладок
+function createTabsComponents(){
+    $(`
+    <div class="tabsContainer"></div>
+    `).appendTo('.innerContiner');
+
+    $(`
+    <nav>
+        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab"
+                aria-controls="nav-home" aria-selected="true">Газы</a>
+            <a class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-profile" role="tab"
+                aria-controls="nav-profile" aria-selected="false">Влагосодержание</a>
+        </div>
+    </nav>
+    `).appendTo('.tabsContainer');
+
+    createChartComponent();
+    createEventArchiveCompoinent();
+}
+
+//Создание компонента кнопки вызывающей боковое меню
+function createBtnCallLeftMenu() {
+    $(`
+    <div class="btnMenu flex-fill bd-highlight">
+        <button type="button" class="btnMenu"></button>
+      </div>
+    `).appendTo('.innerContiner');
+}
+
+//Создание компонентов графика, легенд, таблицы данных под легендами
+function createChartComponent(){
+    $(`
+    <div class="d-flex chart-blockLegends">
+        <div class="chartW">
+            <div class="bg-secondary text-white p-2">График</div>
+            <div class="chart"></div>
+        </div>
+        <div class="legend-tableData">
+            <div class="legends">
+                <div class="bg-secondary text-white p-2">Легенды</div>
+            </div>
+            <div class="dataTable"></div>
+        </div>
+    </div>
+    `).appendTo('.tabsContainer');
+    
+    createLegends(data);
+    createDataTable(dataForTable);
+}
+
+//Создание компонентов архива событий
+function createEventArchiveCompoinent(){
+    $(`
+    <div id="eventArchive">
+        <div class="bg-secondary text-white p-3">Дигностика</div>
+            <div class="d-flex justify-content-around mt-1">
+                <div class="align-self-center">От: тут календарь</div>
+                <div class="align-self-center">До: тут календарь</div>
+                <div class="align-self-center">
+                    <button type="button" class="btn btn-primary">Последний день</button>
+                </div>
+                <div class="align-self-center">
+                    <button type="button" class="btn btn-primary">Применить</button>
+                </div>
+                <div class="align-self-center">
+                    <input type="checkbox" id="diag" value="yes">
+                    <label for="diag">Дигностические</label>
+
+                    <input type="checkbox" id="tech" value="yes">
+                    <label for="tech">Технические</label>
+
+                    <input type="checkbox" id="req" value="yes">
+                    <label for="req">Опрос</label>
+                </div>
+            </div>
+        </div>
+
+        <div></div>
+    </div>
+    `).appendTo('.tabsContainer');
+}
+
+//Демонстрационный график
+function createDemoChart(){
+    // let t1 =[];
+    // let t2 =[];
+
+    // for (let i = 0; i < 10; i++) {
+    //     t1.push(getRandomArbitrary(0, 101));
+    //     t2.push(getRandomArbitrary(0, 101));
+    // }
+
+
+    var traceA = {
+        x: [1, 2, 3, 4, 16, 17, 26],
+        y: [1, 40, 9, 60, 4, 20, 10],
+        type: 'scatter'
+    };
+
+    // var traceA = {
+    //     x: t1,
+    //     y: t2,
+    //     type: 'scatter'
+    // };
+
+    var data = [traceA];
+
+    var layout = {
+        autosize: true,
+        height:500,
+        margin:{
+            pad:5,
+            l:80,
+            t:50,
+            r:10,
+            b:20,
+        },                
+        title: '',
+        xaxis: {
+        },
+        yaxis: {
+            title: 'PPM',
+            //rangemode: 'tozero' выставляет начало осей ровно в 0. если убрать то будут небольшие полоски от 0
+        }
+    };
+    var config = {
+        responsive: true,
+        displayModeBar: false
+    }
+
+    
+    Plotly.newPlot($('.chart').get(0), data, layout, config);
+}
+
+//Создание легенд
+function createLegends(data){
+    $(`
+    <div class="accordion" id="accordionPanelsStayOpenExample"></div>
+    `).appendTo('.legends');
+
+    for (let i = 0; i < data.length; i++) {
+        $(`
+        <div class="accordion-item" id="accItem${i}">
+            <h2 class="accordion-header" id="panelsStayOpen-heading${i}">
+                <button class="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${i}" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    ${data[i].title}
+                </button>
+            </h2>
+        </div>
+        `).appendTo('.accordion');
+
+        for (let j = 0; j < data[i].values.length; j++) {
+            $(`
+            <div id="panelsStayOpen-collapse${i}" class="accordion-collapse collapse " aria-labelledby="panelsStayOpen-heading${i}">
+                <div class="accordion-body">
+                    <input type="checkbox" id="happy" value="yes">
+                    <label for="happy">${data[i].values[j].title}</label>
+                </div>
+            </div>
+            `).appendTo(`#accItem${i}`);
+            
+        }
+        
+    }
+}
+
+//Создание таблицы данных под легендами
+function createDataTable(_dataForTable){
+    $(`
+    <div class="container m-0 p-0">
+        <div class="bg-secondary text-white p-2">Данные</div>
+        <div class="row row-cols-5">
+            <div class="col-2 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark"></div>
+            <div class="col-2 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">ppm</div>
+            <div class="col-4 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">Проецнт изм.</div>
+            <div class="col-2 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">ДЗ</div>
+            <div class="col-2 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">ПДЗ</div>
+        </div>
+    </div>
+    `).appendTo('.dataTable');
+
+    let danger = "bg-danger";
+    let warning = "bg-warning";
+    let normal = "bg-body";
+    let v= "";
+    for (let i = 0; i < _dataForTable.length; i++) {
+        if (_dataForTable[i].ppm >= _dataForTable[i].PDZ){
+            v = danger;
+        }
+        else if (_dataForTable[i].ppm >= _dataForTable[i].DZ && _dataForTable[i].ppm < _dataForTable[i].PDZ){
+            v = warning;
+        }
+        else{
+            v=normal;
+        }
+        $(`
+        <div class="row row-cols-5">
+            <div class="col-2 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">${_dataForTable[i].title}</div>
+            <div class="col-2 p-1 text-center ${v} border border-top-0 border-end-0 border-start-0 border-dark">${_dataForTable[i].ppm}</div>
+            <div class="col-4 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">${_dataForTable[i].percent}</div>
+            <div class="col-2 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">${_dataForTable[i].DZ}</div>
+            <div class="col-2 p-1 text-center border border-top-0 border-end-0 border-start-0 border-dark">${_dataForTable[i].PDZ}</div>
+        </div>
+        `).appendTo('.container');
+        
+    }
+}
+
---
+++ scripts/mainWindow/mainWindow.js
@@ -0,0 +1,76 @@
+
+let states = ["Normal", "Warning", "Critical", "Empty"];
+let disps =[];
+
+function getRandomArbitrary(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+for (let i = 0; i < 15; i++) {
+    let _state = states[getRandomArbitrary(0,4)];
+    let _discription = _state == "Normal" || _state == "Empty" ? "" : "Описание проблемы.......";
+
+    disps.push({title:`AT${i}`, state:_state, discriptionProblem: _discription});
+}
+
+let categories = [
+    {title:'Normal', bg:'bg-success',colorText:'text-white'}, 
+    {title:'Warning', bg:'bg-warning', colorText:'text-white'}, 
+    {title:'Critical', bg:'bg-danger', colorText:'text-white'}, 
+    {title:'Empty', bg:'bg-white', colorText:'text-dark'}];
+
+function createCategory(){
+    $('.innerContiner').empty();
+    $(`
+    <div class="btnMenu">
+        <button type="button" class="btnMenu"></button>
+    </div>
+    `).appendTo('.innerContiner');
+    for (let i = 0; i < categories.length; i++) {
+        $(`<div class="accordion tr" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button ${categories[i].bg} ${categories[i].colorText}" type="button" data-bs-toggle="collapse" 
+                        data-bs-target="#${categories[i].title}" aria-expanded="true" aria-controls="${categories[i].title}"> ${categories[i].title}
+                    </button>
+                </h2>
+                <div id="${categories[i].title}" class="accordion-collapse collapse show" aria-labelledby="headingOne" >
+                    
+                </div>
+            </div>
+        </div>`).appendTo('.innerContiner');
+        
+    }
+}
+
+function fillDispList(){
+    for (let i = 0; i < disps.length; i++) {
+        var categpry = categories.find(q => q.title == disps[i].state);
+        $(`
+        <div class="card ${categpry.bg} ${categpry.colorText} mt-2 mb-2 disp">
+                <div class="card-header d-flex justify-content-between">
+                    ${disps[i].title}
+                    <button type="button" class="btn btn-primary">Данные</button>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${disps[i].state}</h5>
+                    <p class="card-text">${disps[i].discriptionProblem}</p>                    
+                </div>
+        </div>`
+        ).appendTo(`#${disps[i].state}`);
+    }
+}
+
+createCategory();
+fillDispList();
+
+$('.disp').click((e) => {
+    let discriptionDisp =e.currentTarget.innerText.split('\n');
+    // console.log(discriptionDisp[0]);
+    // console.log(window.innerWidth);
+    // console.log(window.innerHeight);
+    
+    loadDisp(discriptionDisp[0])
+});
